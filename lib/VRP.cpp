6858fd69269bba0586aa804e1b3f8c0b1afcf7dc
Edoardo Rosa <edoardo.rosa90@gmail.com>
2015-07-30 02:04:52 +0200

    create one route

--- lib/VRP.cpp
+++ lib/VRP.cpp
@@ -1,12 +1,86 @@
 #include "VRP.h"
 
 /* constructor */
-VRP::VRP(Customer &cust, int n, int v, int c, int t) {
-    this->customers = &cust;
+VRP::VRP(const Graph &g, int n, int v, int c, int t) {
+    this->graph = g;
     this->numVertices = n;
     this->vehicles = v;
     this->capacity = c;
     this->workTime = t;
+    this->InitSolutions();
+}
+
+/* get random, add customers j,j+1,..., n,1,...,j-1 */
+/* check violations of time, capacity */
+/* minimize the increase total time travel */
+void VRP::InitSolutions() {
+    Route r(this->capacity, this->workTime, this->graph);
+    srand(time(NULL));
+    int start;
+    /* distances of customers from depot */
+    Map s = this->graph.sortV0();
+    /* get the depot and remove it from the map */
+    Customer depot = s.begin()->second;
+    s.erase(s.begin());
+    /* keyring of distances multimap s */
+    std::vector<int> keys;
+    Map::iterator it;
+
+    /* choosing a random customer, from 0 to numVertices-1 */
+    start = rand() % (this->numVertices-1);
+    start = 9;
+    it = s.begin();
+    int j = 0;
+    while(it != s.end() && j!=start) {
+        it++;
+        j++;
+    }
+    /* from this customer create routes */
+    this->InsertStep(depot, it, r, s);
+    std::cout << r << std::endl;
+}
+
+bool VRP::InsertStep(Customer depot, Map::iterator &it, Route &r, Map &distances) {
+    Customer from, to;
+    bool flag = true;
+    Map::iterator i = it;
+    /* from depot to random customer j */
+    flag = r.InsertStep(this->graph.GetCosts(depot, i->second), i->second);
+    /* from j to n */
+    while(i != distances.end() && flag) {
+        from = i->second;
+        i++;
+        if (i == distances.end()) {
+            flag = r.InsertStep(this->graph.GetCosts(from, distances.begin()->second), distances.begin()->second);
+            to = from;
+        }else {
+            to = i->second;
+            flag = r.InsertStep(this->graph.GetCosts(from, to), to);
+        }
+    }
+    /* from 0 to j-1 */
+    if (it != distances.begin() && flag) {
+        i = distances.begin();
+        it--;
+        while(i != it && flag) {
+            if (to == from) {
+                to = i->second;
+            }else {
+                from = i->second;
+                i++;
+                if (i != distances.end()) {
+                    to = i->second;
+                    flag = r.InsertStep(this->graph.GetCosts(from, to), to);
+                }
+            }
+        }
+        to = i->second;
+        if (it == i) {
+            flag = r.InsertStep(this->graph.GetCosts(to, from), to);
+        }
+    }
+    flag = r.InsertStep(this->graph.GetCosts(to, depot), depot);
+    return flag;
 }
 
 /* destructor */
--- lib/VRP.h
+++ lib/VRP.h
@@ -1,19 +1,25 @@
 #ifndef VRP_H
 #define VRP_H
 
-#include "../actor/Customer.h"
 #include <iostream>
+#include "Graph.h"
+#include "../actor/Route.h"
+
+typedef std::multimap<int, Customer> Map;
 
 class VRP {
 private:
-    Customer *customers;
+    Graph graph;
+    std::list<Route> routes;
     int numVertices;
     int vehicles;
     int capacity;
     int workTime;
+    bool InsertStep(Customer, Map::iterator &, Route &, Map &);
 public:
     VRP() {}
-    VRP(Customer &cust, int n, int v, int c, int t);
+    VRP(const Graph &g, const int n, const int v, const int c, const int t);
+    void InitSolutions();
     ~VRP();
 };
 
