ad2ea343f426081b79ea956c002feab93d0163d1
Edoardo Rosa <edoardo.rosa90@gmail.com>
2015-08-03 16:34:30 +0200

    private member route

--- actor/Route.cpp
+++ actor/Route.cpp
@@ -2,32 +2,30 @@
 
 /* constructor */
 Route::Route(int c, int wt, const Graph g) {
-    this->capacity = c;
-    this->workTime = wt;
+    this->initialCapacity = this->capacity = c;
+    this->initialWorkTime = this->workTime = wt;
     this->graph = g;
     this->totalCost = 0;
 }
 
-void Route::CloseTravel(Customer c) {
+void Route::CloseTravel(const Customer c) {
     Customer depot = this->route.cbegin()->first;
     int costToDepot = this->graph.GetCosts(c, depot).second;
+    this->totalCost += costToDepot;
     this->route.push_back({c, costToDepot});
     this->route.push_back({depot, 0});
 }
 
-bool Route::CloseTravel(Customer from, Customer depot) {
+bool Route::CloseTravel(const Customer from, const Customer depot) {
     bool ret = true;
     // save the route state
     int tCost = this->totalCost;
     int workT = this->workTime;
-    // check constraints
     int travelCost = this->graph.GetCosts(from, depot).second;
     tCost += travelCost;
-    workT -= (travelCost * this->TRAVEL_COST);
     // must consider the time to return to depot
-    // after the travel if constraints fails
+    workT -= (travelCost * this->TRAVEL_COST);
     if (workT < 0) {
-        // no time or capacity to serve the customer: return to depot
         ret = false;
     } else {
         // the travel can be added to the route
@@ -39,16 +37,16 @@ bool Route::CloseTravel(Customer from, Customer depot) {
     return ret;
 }
 
-bool Route::Travel(Customer from, Customer to) {
+bool Route::Travel(const Customer from, const Customer to) {
     Customer depot;
     bool ret = true;
-    int returnTime = 0;
+    int returnTime;
     // save the route state
     int tCost = this->totalCost;
     int capac = this->capacity;
     int workT = this->workTime;
-    // check constraints
     int travelCost = this->graph.GetCosts(from, to).second;
+    // get the depot
     if (this->route.empty()) {
         depot = from;
     }else {
@@ -67,21 +65,21 @@ bool Route::Travel(Customer from, Customer to) {
     } else {
         // the travel can be added to the route
         this->totalCost = tCost;
-        this->capacity = capacity;
+        this->capacity = capac;
         this->workTime = workT;
         this->route.push_back({from, travelCost});
     }
     return ret;
 }
 
-void Route::EmptyRoute(Customer depot) {
+void Route::EmptyRoute(const Customer depot) {
     this->route.clear();
     this->route.push_back({depot, 0});
 }
 
 void Route::PrintRoute() {
     std::flush(std::cout);
-    if (this->route.size() > 0) {
+    if (this->route.size() > 1) {
         for (StepType i : this->route) {
             if (i.second > 0)
                 std::cout << i.first << " -(" << i.second << ")-> ";
@@ -93,4 +91,27 @@ void Route::PrintRoute() {
         std::cout << "Void Route!" << std::endl;
     }
     std::flush(std::cout);
+}
+
+// how good is a route
+void Route::SetFitness() {
+    // route with 1, is perfect
+    float cWeight = 6.0f;
+    float tWeight = 8.0f;
+    float cRate = 1.0f - (float)this->capacity / (float)this->initialCapacity;
+    float tRate = 1.0f - (float)this->workTime / (float)this->initialWorkTime;
+    this->fitness = (cRate * cWeight + tRate * tWeight) / (cWeight + tWeight);
+}
+
+int Route::size() const {
+    return this->route.size();
+}
+
+std::list<StepType>* Route::GetRoute() {
+    return &this->route;
+}
+
+bool Route::MoveCustomer(const Customer c) {
+    bool ret = true;
+    return ret;
 }
--- actor/Route.h
+++ actor/Route.h
@@ -10,33 +10,45 @@ class Route {
 private:
     /* overloading << */
     friend std::ostream& operator<<(std::ostream& out, const Route &r) {
-        std::flush(std::cout);
-        for (auto i : r.route)
-            if (i.second > 0)
-                out << i.first << " -(" << i.second << ")-> ";
-            else
-                out << i.first;
+        if (r.route.size() > 1) {
+            std::flush(std::cout);
+            for (auto i : r.route)
+                if (i.second > 0)
+                    out << i.first << " -(" << i.second << ")-> ";
+                else
+                    out << i.first;
+        }
         return out;
     }
+    int initialCapacity;
     int capacity;
+    int initialWorkTime;
     int workTime;
     int totalCapacity;
     int totalTime;
     int totalCost;
+    // cost of traveling
     const float TRAVEL_COST = 0.3;
     Graph graph;
 protected:
-
-public:
-    void CloseTravel(Customer);
-    bool CloseTravel(Customer, Customer);
-    bool CheckConstraints(Customer, Customer);
-    void PrintRoute();
-    bool Travel(Customer, Customer);
-    void EmptyRoute(Customer);
-    /* per ora list, alternativa: deque */
     std::list<StepType> route;
+public:
+    float fitness;
+    /* constructor */
     Route(int, int, const Graph);
+    // add the travel from the customer to the depot
+    void CloseTravel(const Customer);
+    // add the travel from the first customer to the second, then to the depot
+    bool CloseTravel(const Customer, const Customer);
+    void PrintRoute();
+    // insert the travel from customer A to B, if possibile
+    bool Travel(const Customer, const Customer);
+    // create an empty route
+    void EmptyRoute(const Customer);
+    void SetFitness();
+    int size() const;
+    std::list<StepType>* GetRoute();
+    bool MoveCustomer(const Customer);
 };
 
 #endif /* Route_H */
