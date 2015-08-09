289fb2601059bacf06fd63e9b595b3279bec8a3a
Edoardo Rosa <edoardo.rosa90@gmail.com>
2015-08-10 00:28:16 +0200

    new colors

--- lib/Utils.h
+++ lib/Utils.h
@@ -19,9 +19,11 @@ private:
     void operator=(Utils const&) = delete;
     rapidjson::Document d;
     const char* ANSI_RESET = "\u001B[0m";
-    const char* ANSI_RED = "\u001B[31m";
-    const char* ANSI_GREEN = "\u001B[32m";
+    const char* ANSI_RED = "\u001B[1;31m";
+    const char* ANSI_GREEN = "\u001B[1;32m";
     const char* ANSI_YELLOW = "\u001B[33m";
+    const char* ANSI_BLUE = "\u001B[34m";
+    const char* ANSI_LIGHTGREEN = "\u001B[32m";
 public:
     static Utils& Instance() {
         static Utils instance;
@@ -30,19 +32,23 @@ public:
     static const int ERROR = 1;
     static const int WARNING = 2;
     static const int SUCCESS = 0;
+    static const int NORMAL = 3;
     VRP* InitParameters (char **);
     FILE* SaveResult(void);
     template <typename T>
-    void logger(T s, int c) const {
+    void logger(T s, int c = NORMAL) const {
         switch(c) {
             case SUCCESS:
-                std::cout << ANSI_GREEN << s << ANSI_RESET << std::endl;
+                std::cout << ANSI_LIGHTGREEN << s << ANSI_RESET << std::endl;
             break;
             case WARNING:
                 std::cout << ANSI_YELLOW << s << ANSI_RESET << std::endl;
             break;
             case ERROR:
                 std::cout << ANSI_RED << s << ANSI_RESET << std::endl;
+            break;
+            default:
+                std::cout << s << std::endl;
             break;
         }
     }
