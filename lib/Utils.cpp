02cf6b8c365014bb6da866220f070881e958906f
Edoardo Rosa <edoardo.rosa90@gmail.com>
2015-10-05 23:18:43 +0200

    fix license

--- lib/Utils.cpp
+++ lib/Utils.cpp
@@ -12,7 +12,7 @@
     GNU General Public License for more details.
 
     You should have received a copy of the GNU General Public License
-    along with Nome-Programma.  If not, see <http://www.gnu.org/licenses/>.
+    along with VRP.  If not, see <http://www.gnu.org/licenses/>.
  ****************************************************************************/
 
 #include "Utils.h"
